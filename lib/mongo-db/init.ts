3226f6f01f95b8a686b97564f62e52b1112076bd
chi1180 <cwchi1@outlook.jp>
2025-06-01 14:22:42 +0900

    config uri changed

--- lib/mongo-db/init.ts
+++ lib/mongo-db/init.ts
@@ -1,3 +1,3 @@
-const uri = `mongodb+srv://${encodeURIComponent(process.env.DB_USER || "")}:${encodeURIComponent(process.env.DB_PASSWORD || "")}@stocks.q8sgkmk.mongodb.net/?retryWrites=true&w=majority&appName=stocks`;
+const uri = `mongodb+srv://${encodeURIComponent(process.env.DB_USER || "")}:${encodeURIComponent(process.env.DB_PASSWORD || "")}@stocks.q8sgkmk.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true&authSource=admin&appName=stocks`;
 
 export { uri };

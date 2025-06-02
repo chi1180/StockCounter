d727b872439bc9c9ca345e34d781e1f932ad9afb
chi1180 <cwchi1@outlook.jp>
2025-06-02 21:34:45 +0900

    api fun changed

--- lib/mongo-db/get.ts
+++ lib/mongo-db/get.ts
@@ -1,19 +1,19 @@
 import { MongoClient } from "mongodb";
 import { uri } from "./init";
 
-async function get(query: object) {
+async function get(query: { type: string; [key: string]: any }) {
   const client = new MongoClient(uri);
 
   try {
     const database = client.db("stocks");
-    const collection = database.collection("bought");
+    const collectionName = query.type === "goods" ? "goods" : "bought";
+    const collection = database.collection(collectionName);
     const result = await collection.findOne(query);
     return result;
   } catch (error) {
-    console.log(`[--ERROR--] Failed to load data\n${error}`);
+    console.error(`[--ERROR--] Failed to load data\n${error}`);
+    throw error; // エラーを上位に伝播させる
   } finally {
     await client.close();
   }
 }
-
-export { get };
